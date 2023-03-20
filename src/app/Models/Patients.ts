0f4c688ca6a43bb554f8ceb330a063f461055ea2
Ravi Teja Takkellapati <ravimahiind@gmail.com>
2023-03-20 12:24:39 -0400

    Initial commit

---
+++ src/app/Models/Patients.ts
@@ -0,0 +1,12 @@
+export class Patients {
+    patientID: any;
+    firstName?: string;
+    lastName?: string;
+    fullName?: string;
+    dob?: Date;
+    gender?: string;
+    age?: number;
+    patientAddress?: string;
+    phoneNumber?: number;
+    emailAddress?: string;
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,57 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { LoginComponent } from './login/login.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ToastrModule } from 'ngx-toastr';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule, Routes } from '@angular/router';
+import { JwtModule } from '@auth0/angular-jwt';
+import { AuthGuard } from './auth/authentication.service';
+import { PatientsComponent } from './patients/patients.component';
+import { HttpClientModule } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+
+//all components routes
+const routes: Routes = [
+  { path: '', component: HomepageComponent },
+  { path: 'app-patients', component: PatientsComponent, canActivate: [AuthGuard] },
+  { path: 'app-login', component: LoginComponent },
+];
+
+//function is use to get jwt token from local storage
+export function tokenGetter() {
+  return localStorage.getItem("jwt");
+}
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    LoginComponent,
+    PatientsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    RouterModule.forRoot(routes),
+    JwtModule.forRoot({
+      config: {
+        tokenGetter: tokenGetter,
+        allowedDomains: ["localhost:7299"],
+        disallowedRoutes: []
+      }
+    }),
+    ToastrModule.forRoot()
   ],
-  providers: [],
+  providers: [AuthGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
